faa8b092013a6c53b2f9d5367b8af70dbbbd42b2
thun888 <2238342947@qq.com>
2023-08-09 23:39:05 +0800

    兼容新加密

--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ rm mirouterui_linux_mipsle
 
 echo "Building win_amd64"
 GOOS=windows GOARCH=amd64 go build main.go
-#mv main.exe mirouterui_win_amd64.exe
-upx --best -o mirouterui_win_amd64.exe main.exe
+mv main.exe mirouterui_win_amd64.exe
+upx --best -o mirouterui_win_amd64_upx.exe mirouterui_win_amd64.exe
 echo "Building win_386"
 GOOS=windows GOARCH=386 go build main.go
-# mv main.exe mirouterui_win_386.exe
-upx --best -o mirouterui_win_386.exe main.exe
+mv main.exe mirouterui_win_386.exe
+upx --best -o mirouterui_win_386_upx.exe mirouterui_win_386.exe
 echo "Building win_arm64"
 GOOS=windows GOARCH=arm64 go build main.go
 mv main.exe mirouterui_win_arm64.exe
@@ -22,23 +22,25 @@ mv main.exe mirouterui_win_arm64.exe
 
 echo "Building linux_amd64"
 GOOS=linux GOARCH=amd64 go build main.go
-# mv main main_linux_amd64
-upx --best -o mirouterui_linux_amd64 main
+mv main main_linux_amd64
+upx --best -o mirouterui_linux_amd64_upx main_linux_amd64
 echo "Building linux_arm64"
 GOOS=linux GOARCH=arm64 go build main.go
-# mv main main_linux_arm64
-upx --best -o mirouterui_linux_arm64 main
+mv main main_linux_arm64
+upx --best -o mirouterui_linux_arm64_upx main_linux_arm64
 echo "Building linux_mipsle"
 GOOS=linux GOARCH=mipsle GOMIPS=softfloat go build main.go
-# mv main main_linux_mipsle
-upx --best -o mirouterui_linux_mipsle main
+mv main main_linux_mipsle
+upx --best -o mirouterui_linux_mipsle_upx main_linux_mipsle
 rm main
 
 echo "Building darwin_amd64"
 GOOS=darwin GOARCH=amd64 go build main.go
-upx --best -o mirouterui_darwin_amd64 main
+mv main main_darwin_amd64
+upx --best -o mirouterui_darwin_amd64_upx main_darwin_amd64
 echo "Building darwin_arm64"
 GOOS=darwin GOARCH=arm64 go build main.go
-upx --best -o mirouterui_darwin_arm64 main
+mv main main_darwin_arm64
+upx --best -o mirouterui_darwin_arm64_upx main_darwin_arm64
 rm main
 
--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"crypto/sha1"
+	"crypto/sha256"
 	"embed"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -29,7 +32,13 @@ var (
 var static embed.FS
 
 func init() {
-	data, err := ioutil.ReadFile("config.txt")
+	exePath, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	configPath := filepath.Join(filepath.Dir(exePath), "config.txt")
+	fmt.Println(configPath)
+	data, err := os.ReadFile(configPath)
 	if err != nil {
 		panic(err)
 	}
@@ -38,6 +47,7 @@ func init() {
 	key = strings.Split(strings.Split(config, `key = "`)[1], `"`)[0]
 	iv = strings.Split(strings.Split(config, `iv = "`)[1], `"`)[0]
 	ip = strings.Split(strings.Split(config, `ip = "`)[1], `"`)[0]
+
 	// fmt.Println(password)
 	// fmt.Println(key)
 	// fmt.Println(iv)
@@ -46,7 +56,7 @@ func init() {
 
 func createNonce() string {
 	typeVar := 0
-	deviceID := "2a:45:6f:11:0b:a5"
+	deviceID := "fc:34:97:69:cc:06"
 	timeVar := int(time.Now().Unix())
 	randomVar := rand.Intn(10000)
 	return fmt.Sprintf("%d_%s_%d_%d", typeVar, deviceID, timeVar, randomVar)
@@ -65,10 +75,65 @@ func hashPassword(pwd string, nonce string, key string) string {
 
 	return noncePwdKeyHashStr
 }
+func newhashPassword(pwd string, nonce string, key string) string {
+	pwdKey := pwd + key
+	pwdKeyHash := sha256.Sum256([]byte(pwdKey))
+	pwdKeyHashStr := hex.EncodeToString(pwdKeyHash[:])
+
+	noncePwdKey := nonce + pwdKeyHashStr
+	noncePwdKeyHash := sha256.Sum256([]byte(noncePwdKey))
+	noncePwdKeyHashStr := hex.EncodeToString(noncePwdKeyHash[:])
+
+	return noncePwdKeyHashStr
+}
+func getrouterinfo() int {
+
+	// 发送 GET 请求
+	ourl := fmt.Sprintf("http://%s/cgi-bin/luci/api/xqsystem/login", ip)
+	response, err := http.Get(ourl)
+	if err != nil {
+		return 0
+	}
+	defer response.Body.Close()
+
+	// 读取响应内容
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return 0
+	}
+
+	// 解析 JSON
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return 0
+	}
+
+	// 检查 newEncryptMode
+	newEncryptMode, ok := data["newEncryptMode"].(float64)
+	if !ok {
+		return 0
+	}
+
+	if newEncryptMode != 0 {
+		return 1
+	}
+
+	return 0
+}
 func updateToken() {
+	fmt.Println("获取路由器信息...")
+	newEncryptMode := getrouterinfo()
 	fmt.Println("更新token...")
 	nonce := createNonce()
-	hashedPassword := hashPassword(password, nonce, key)
+	var hashedPassword string
+
+	if newEncryptMode == 1 {
+		hashedPassword = newhashPassword(password, nonce, key)
+	} else {
+		hashedPassword = hashPassword(password, nonce, key)
+	}
+
 	ourl := fmt.Sprintf("http://%s/cgi-bin/luci/api/xqsystem/login", ip)
 	params := url.Values{}
 	params.Set("username", "admin")
@@ -84,16 +149,17 @@ func updateToken() {
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, _ := io.ReadAll(resp.Body)
 	var result map[string]interface{}
 	json.Unmarshal(body, &result)
 	code := int(result["code"].(float64))
 	if code == 0 {
-		// fmt.Println(result)
+		fmt.Println("当前token为:", result["token"])
 		token = result["token"].(string)
 	} else {
 		fmt.Println("登录失败，请检查配置")
 		fmt.Println("30秒后退出程序")
+		fmt.Println(string(body))
 		time.Sleep(30 * time.Second)
 		os.Exit(1)
 	}
@@ -117,7 +183,7 @@ func main() {
 				})
 			}
 			defer resp.Body.Close()
-			body, _ := ioutil.ReadAll(resp.Body)
+			body, _ := io.ReadAll(resp.Body)
 			var result map[string]interface{}
 			json.Unmarshal(body, &result)
 			return c.JSON(http.StatusOK, result)
