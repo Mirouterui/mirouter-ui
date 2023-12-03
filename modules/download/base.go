7ef096dcea252a30a391dc45327aa552f9d20d37
thun888 <2238342947@qq.com>
2023-12-03 08:56:42 +0800

    Optimize static resource download logic

--- modules/download/base.go
+++ modules/download/base.go
@@ -2,6 +2,7 @@ package download
 
 import (
 	"archive/zip"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -25,22 +26,59 @@ func DownloadStatic(basedirectory string, force bool) error {
 	_, err := os.Stat(directory)
 	if os.IsNotExist(err) || force {
 		logrus.Info("正从'Mirouterui/static'下载静态资源")
-		resp, err := http.Get("http://mrui-api.hzchu.top/downloadstatic")
-		checkErr(err)
-		defer resp.Body.Close()
-
-		out, err := os.CreateTemp("", "*.zip")
-		checkErr(err)
-		defer out.Close()
-
-		_, err = io.Copy(out, resp.Body)
-		checkErr(err)
-
-		err = unzip(out.Name(), directory)
-		checkErr(err)
+		downloadfile(directory)
+		return nil
 	}
 
+	// 读取/static/version/index.html
+	f, err := os.Open(filepath.Join(directory, "version", "index.html"))
+	if err != nil {
+		logrus.Info("无法读取静态资源版本号，重新下载")
+		os.RemoveAll(directory)
+		downloadfile(directory)
+		return err
+	}
+
+	defer f.Close()
+	forntendVersion, err := io.ReadAll(f)
+	checkErr(err)
+	logrus.Info("静态资源已存在，版本号为" + string(forntendVersion))
+
+	resp, err := http.Get("https://mrui-api.hzchu.top/checkupdate")
+	if err != nil {
+		logrus.Info("无法获取更新信息，重新下载")
+		os.RemoveAll(directory)
+		downloadfile(directory)
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	checkErr(err)
+	var result map[string]interface{}
+	json.Unmarshal(body, &result)
+
+	if result["frontversion"] != string(forntendVersion) {
+		logrus.Info("发现新版本，重新下载")
+		os.RemoveAll(directory)
+		downloadfile(directory)
+	}
 	return nil
+}
+func downloadfile(directory string) {
+	resp, err := http.Get("http://mrui-api.hzchu.top/downloadstatic")
+	checkErr(err)
+	defer resp.Body.Close()
+
+	out, err := os.CreateTemp("", "*.zip")
+	checkErr(err)
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	checkErr(err)
+
+	err = unzip(out.Name(), directory)
+	checkErr(err)
 }
 
 func unzip(src, dest string) error {
